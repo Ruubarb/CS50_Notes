08210d5524b223ace3b11e3380bb0e162d01d10f
00saad <00saadchaudhry@gmail.com>
2018-01-21 22:48:59 -0500

    Code is possibly figured out. Verdict image uploaded

--- Week 4 - Memory/PSet4/whodunit/whodunit.c
+++ Week 4 - Memory/PSet4/whodunit/whodunit.c
@@ -76,11 +76,12 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0xff && triple.rgbtRed == 0x00)
+            //if the pixels are red, turn them off (in reverse hexadecimal order)
+            if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00 && triple.rgbtRed == 0xff)
             {
                 triple.rgbtBlue = 0xff;
-                triple.rgbtGreen = 0x00;
-                triple.rgbtRed = 0x00;
+                triple.rgbtGreen = 0xff;
+                triple.rgbtRed = 0xff;
             }
 
             // write RGB triple to outfile
