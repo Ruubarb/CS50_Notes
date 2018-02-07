367414e242eca3c91c532f6aeaf4122fefb67556
00saad <00saadchaudhry@gmail.com>
2018-02-07 09:30:54 -0500

    Working on padding

--- Week 4 - Memory/PSet4/resize/resize.c
+++ Week 4 - Memory/PSet4/resize/resize.c
@@ -93,16 +93,19 @@ int main(int argc, char *argv[])
                 // write RGB triple to outfile
                 fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
             }
+
+        }
+
+        // then add it back (to demonstrate how)
+        for (int k = 0; k < newPadding; k++)
+        {
+            fputc(0x00, outptr);
         }
 
         // skip over padding, if any
         fseek(inptr, padding, SEEK_CUR);
 
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
+
     }
 
     // close infile
