2b4731aeb3d8d5142f2ec11d05c48a596a33a18e
00Saad <00saadchaudhry@gmail.com>
2018-02-22 10:03:01 -0500

    Started recover.c with only basic code. Updated gitignore

---
+++ Week 4 - Memory/PSet4/recover/recover.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+
+    char *infile = argv[1];
+
+    FILE *memcard = fopen("card.raw", "r");
+    if (memcard == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    fclose(memcard);
+
+    return 0;
+}
