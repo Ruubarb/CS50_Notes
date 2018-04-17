ccfe13143b77d6f9dd0be19269ed036f160be97e
00Saad <00saadchaudhry@gmail.com>
2018-04-17 17:18:37 -0400

    Created a basic linked list, but without using a loop

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -10,7 +10,19 @@ int main(void)
         struct node* next;
     }
     node;
+
     node* head = malloc(sizeof(node));
+    node* second = malloc(sizeof(node));
+    node* third = malloc(sizeof(node));
+
+    head->value = 1;
+    head->next = second;
+    second->value = 2;
+    second->next = third;
+    third->value = 3;
+    third->next = NULL;
+
+    printf("%d\n", third->value);
 
     return 0;
 }
