b65039a276424285b8fcc7710d73e5e565f61d51
00Saad <00saadchaudhry@gmail.com>
2018-04-23 17:21:51 -0400

    Learned how to traverse linked list and reach value of final node

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -19,6 +19,19 @@ int main(void)
     head->next = cursor; //head node points cursor
     cursor->value = 2;
     cursor->next = NULL;
+    cursor->value = 4;
+    cursor->next = NULL;
+    cursor->value = 6;
+    cursor->next = NULL;
+
+    //creating cursor1 node and while loop allows us to traverse list until reaching final value
+    node* cursor1 = head;
+    while (cursor1->next != NULL)
+    {
+        cursor1 = cursor1->next;
+        printf("%i\n", cursor1->value);
+    }
+
 
     while (head != NULL)
     {
