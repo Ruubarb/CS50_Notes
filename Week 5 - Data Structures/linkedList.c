d01d2392647cc2c254d581dfb032dc761611f25b
00Saad <00saadchaudhry@gmail.com>
2018-04-23 17:23:15 -0400

    Removed commented out code

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -40,32 +40,5 @@ int main(void)
         free(temp);
     }
 
-    /*
-    node* head = malloc(sizeof(node));
-    node* second = malloc(sizeof(node));
-    node* third = malloc(sizeof(node));
-
-    head->value = 1;
-    head->next = second;
-    second->value = 2;
-    second->next = third;
-    third->value = 3;
-    third->next = NULL;
-
-    printf("%d\n", second->value);
-
-
-
-    node* cursor = malloc(sizeof(node));
-    cursor->value = 1;
-
-    while(cursor != NULL)
-    {
-        printf("%d\n", cursor->value);
-        cursor->value = cursor->value+1;
-        cursor = cursor->next;
-    }
-    */
-
     return 0;
 }
