542dcccad239f63950ec6fe69f7dca738aa64674
00Saad <00saadchaudhry@gmail.com>
2018-04-27 19:04:48 -0400

    Used someone else's code to finish a linked list program that creates nodes from 1-10. I understand 80% of the program, but not the rest

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -15,29 +15,43 @@ int main(void)
     node* temp = NULL;
     node* content = NULL;
 
-    //int i;
+    int i;
 
-    content = malloc(sizeof(node));
-    content->value = 1;
-    content->next = NULL;
-    head = content;
-    printf("%i\n", content->value);
+    /*
+    code from https://stackoverflow.com/a/50012170/7217977, written by Srini
+    */
 
-    content = malloc(sizeof(node));
-    content->value = 2;
-    content->next = content;
-    head = content;
-    printf("%i\n", content->value);
+    for (i = 1; i <= 10; i++)
+    {
+        if (head == NULL)
+        {
+            // The list is empty when this condition triigers
+            // we initialize the first node and assign head to point to it
+            head = (node*)malloc(sizeof(node)); // allocate memory to the pointer. Also make sure the memory pointed to is of a node type
+                                                // This is important because it will allow us use -> operator to assign values to the pointed
+                                                // at memory
+            head -> value = i; // assign the value i to the value field of the pointed to memory
+            head -> next = NULL; // assign the next pointer of the pointed to memory to NULL
+        }
+        else
+        {
+            // iterate over the list till we reach the end.
+            // Once we do, assign more memory at the end, assign the memory a value and make it's next pointer be NULL
+            content = head;
+            while (content -> next != NULL)
+            {
+                content = content -> next;
+            }
+            content -> next = (node*)malloc(sizeof(node));
+            content -> next -> value = i;
+        }
 
-    content = malloc(sizeof(node));
-    content->value = 3;
-    content->next = content;
-    head = content;
-    printf("%i\n", content->value);
+    }
 
     while (head != NULL)
     {
         temp = head;
+        printf("Node data: %d\n", temp -> value);
         head = head->next;
         free(temp);
     }
