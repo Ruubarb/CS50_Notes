c2c8b3b13a165bbfb9eeb674969412706b6f770b
00Saad <00saadchaudhry@gmail.com>
2018-04-27 19:08:00 -0400

    Refactored code

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -25,25 +25,28 @@ int main(void)
     {
         if (head == NULL)
         {
-            // The list is empty when this condition triigers
+            // The list is empty when this condition triggers
             // we initialize the first node and assign head to point to it
-            head = (node*)malloc(sizeof(node)); // allocate memory to the pointer. Also make sure the memory pointed to is of a node type
-                                                // This is important because it will allow us use -> operator to assign values to the pointed
-                                                // at memory
-            head -> value = i; // assign the value i to the value field of the pointed to memory
-            head -> next = NULL; // assign the next pointer of the pointed to memory to NULL
+            /*
+            allocate memory to the pointer. Also make sure the memory pointed to is of a node type
+            This is important because it will allow us to use the -> operator to assign values to the pointed
+            at memory
+            */
+            head = malloc(sizeof(node));
+            head->value = i; // assign the value i to the value field of the pointed to memory
+            head->next = NULL; // assign the next pointer of the pointed to memory to NULL
         }
         else
         {
-            // iterate over the list till we reach the end.
+            // iterate over the list until we reach the end.
             // Once we do, assign more memory at the end, assign the memory a value and make it's next pointer be NULL
             content = head;
-            while (content -> next != NULL)
+            while (content->next != NULL)
             {
-                content = content -> next;
+                content = content->next;
             }
-            content -> next = (node*)malloc(sizeof(node));
-            content -> next -> value = i;
+            content->next = malloc(sizeof(node));
+            content->next->value = i; //content points to next, the next pointer in content points to value, the value points to i
         }
 
     }
@@ -51,7 +54,7 @@ int main(void)
     while (head != NULL)
     {
         temp = head;
-        printf("Node data: %d\n", temp -> value);
+        printf("Node data: %d\n", temp->value);
         head = head->next;
         free(temp);
     }
