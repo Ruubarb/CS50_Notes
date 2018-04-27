21aab8f9f15dc0614d26d38ca1ae1681ca495285
00Saad <00saadchaudhry@gmail.com>
2018-04-27 15:34:24 -0400

    Removed old, commented-out code. Creating program that adds nodes to beginning of linked list

--- Week 5 - Data Structures/linkedList.c
+++ Week 5 - Data Structures/linkedList.c
@@ -19,15 +19,10 @@ int main(void)
 
     for (i = 1; i <= 10; i++)
     {
-        content = malloc(sizeof(head));
+        content = malloc(sizeof(node));
         content->value = i;
-        content->next = head;
         head = content;
-        while (content->next != NULL)
-        {
-            printf("%i ", content->value);
-            content = content->next;
-        }
+        content->next = head;
     }
     printf("\n");
 
@@ -37,40 +32,6 @@ int main(void)
         head = head->next;
         free(temp);
     }
-
-    /*
-    node* head = NULL;
-    head = malloc(sizeof(node));
-
-    node* cursor = malloc(sizeof(node));
-
-    head->next = cursor; //head node points cursor
-
-
-    cursor->value = 2;
-    cursor->next = NULL;
-    cursor->value = 4;
-    cursor->next = NULL;
-    cursor->value = 6;
-    cursor->next = NULL;
-
-    //creating cursor1 node and while loop allows us to traverse list until reaching final value
-    node* cursor1 = head;
-    while (cursor1->next != NULL)
-    {
-        cursor1 = cursor1->next;
-        printf("%i\n", cursor1->value);
-    }
-
-
-    for (int i = 1; i <= 10; i++)
-    {
-        cursor->value = i;
-        cursor->next = NULL;
-        printf("%i\n", cursor->value);
-    }
-    */
-
 
     return 0;
 }
